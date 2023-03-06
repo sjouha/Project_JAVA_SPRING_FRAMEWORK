98d55be3f2c89d41c1bd97e2a07a3e7fccec5cb5
Soufiane Jouhar <s.jouhar@nextskills.ma>
2023-03-07 00:25:44 +0100

    init commit

---
+++ src/test/java/dwfsd/tps/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dwfsd.tps.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
