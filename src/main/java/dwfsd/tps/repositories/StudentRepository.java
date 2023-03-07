bc544403aa8cd5b182c6230c06034f737ba34279
Soufiane Jouhar <s.jouhar@nextskills.ma>
2023-03-07 16:33:00 +0100

    init commit

--- src/main/java/dwfsd/tps/repositories/StudentRepository.java
+++ src/main/java/dwfsd/tps/repositories/StudentRepository.java
@@ -3,27 +3,58 @@ package dwfsd.tps.repositories;
 import dwfsd.tps.entities.StudentEntity;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class StudentRepository implements IStudentRepository{
+    @PersistenceContext
+    EntityManager entityManager;
     @Override
-    public StudentEntity save(StudentEntity dto) {
-        return null;
+    public StudentEntity save(StudentEntity entity) {
+        entityManager.persist(entity);
+        StudentEntity studentEntity = entityManager.find(StudentEntity.class, entity.getId());
+        if(!Objects.isNull(studentEntity)) {
+            return studentEntity;
+        }
+        return new StudentEntity();
     }
 
     @Override
-    public StudentEntity update(StudentEntity dto) {
-        return null;
+    public StudentEntity update(StudentEntity entity) {
+        StudentEntity studentEntity = entityManager.find(StudentEntity.class, entity.getId());
+        if(!Objects.isNull(studentEntity)) {
+            studentEntity.setName(entity.getName());
+            entityManager.persist(studentEntity);
+            return studentEntity;
+        }
+        return new StudentEntity();
     }
 
     @Override
     public boolean delete(Long id) {
+        StudentEntity studentEntity = entityManager.find(StudentEntity.class, id);
+        if(!Objects.isNull(studentEntity)) {
+            entityManager.remove(studentEntity);
+            return true;
+        }
         return false;
     }
 
     @Override
     public List<StudentEntity> selectAll() {
-        return null;
+        Query query = entityManager.createQuery("SELECT * FROM STUDENTS");
+        try {
+             if(!query.getResultList().isEmpty()) {
+                 return query.getResultList();
+             }
+        } catch (Exception exception) {
+            System.err.println("Error :" + exception.getMessage());
+        }
+        return new ArrayList<>();
     }
 }
