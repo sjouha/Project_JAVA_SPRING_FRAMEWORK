eafe0d7739c21d6bbb63333917cfbd70f3ead7cd
Soufiane Jouhar <s.jouhar@nextskills.ma>
2023-03-07 16:39:07 +0100

    init commit

--- src/main/java/dwfsd/tps/repositories/StudentRepository.java
+++ src/main/java/dwfsd/tps/repositories/StudentRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -16,45 +15,21 @@ public class StudentRepository implements IStudentRepository{
     EntityManager entityManager;
     @Override
     public StudentEntity save(StudentEntity entity) {
-        entityManager.persist(entity);
-        StudentEntity studentEntity = entityManager.find(StudentEntity.class, entity.getId());
-        if(!Objects.isNull(studentEntity)) {
-            return studentEntity;
-        }
-        return new StudentEntity();
+        return null;
     }
 
     @Override
     public StudentEntity update(StudentEntity entity) {
-        StudentEntity studentEntity = entityManager.find(StudentEntity.class, entity.getId());
-        if(!Objects.isNull(studentEntity)) {
-            studentEntity.setName(entity.getName());
-            entityManager.persist(studentEntity);
-            return studentEntity;
-        }
-        return new StudentEntity();
+        return null;
     }
 
     @Override
     public boolean delete(Long id) {
-        StudentEntity studentEntity = entityManager.find(StudentEntity.class, id);
-        if(!Objects.isNull(studentEntity)) {
-            entityManager.remove(studentEntity);
-            return true;
-        }
         return false;
     }
 
     @Override
     public List<StudentEntity> selectAll() {
-        Query query = entityManager.createQuery("SELECT * FROM STUDENTS");
-        try {
-             if(!query.getResultList().isEmpty()) {
-                 return query.getResultList();
-             }
-        } catch (Exception exception) {
-            System.err.println("Error :" + exception.getMessage());
-        }
-        return new ArrayList<>();
+        return null;
     }
 }
