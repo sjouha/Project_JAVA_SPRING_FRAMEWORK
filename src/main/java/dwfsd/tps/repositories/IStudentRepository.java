899624d24aafbc82dc685d2b938f2e4981706d7a
Soufiane Jouhar <s.jouhar@nextskills.ma>
2023-03-09 23:36:02 +0100

    init commit

--- src/main/java/dwfsd/tps/repositories/IStudentRepository.java
+++ src/main/java/dwfsd/tps/repositories/IStudentRepository.java
@@ -3,10 +3,11 @@ package dwfsd.tps.repositories;
 
 
 import dwfsd.tps.entities.StudentEntity;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface IStudentRepository {
+public interface IStudentRepository extends CrudRepository<StudentEntity, Long> {
 
     StudentEntity save(StudentEntity entity);
 
